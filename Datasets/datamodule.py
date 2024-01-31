a42e667a1210a644d40a187e0919b457bb9efa20
Lukas Starenko <31926276+Lukasis150@users.noreply.github.com>
2024-01-31 14:53:28 -0300

    add prints

--- Datasets/datamodule.py
+++ Datasets/datamodule.py
@@ -27,6 +27,20 @@ class BaseDataModule(LightningDataModule):
             f"Loading Dataset : {self.config.dataset_name}, language : {self.config.subset}"
         )
 
+        # dataset_config = {
+        #     "LOADING_SCRIPT_FILES": os.path.join(PROJECT_ROOT, "crema.py"),
+        #     "CONFIG_NAME": "clean",
+        #     "DATA_DIR": os.path.join(PROJECT_ROOT, "data/archive.zip"),
+        #     "CACHE_DIR": os.path.join(PROJECT_ROOT, "cache_crema"),
+        # }
+
+        # ds = load_dataset(
+        #     dataset_config["LOADING_SCRIPT_FILES"],
+        #     dataset_config["CONFIG_NAME"],
+        #     data_dir=dataset_config["DATA_DIR"],
+        #     cache_dir=dataset_config["CACHE_DIR"]
+        # )
+
     def prepare_data(self) -> None:
         return super().prepare_data()
 
@@ -72,6 +86,11 @@ class BaseDataModule(LightningDataModule):
             #         f"Loaded {split} dataset : {name_file_path}")
             # else:
             # except
+            print(self.config.dataset_name)
+            print(self.config.subset)
+            print(self.config.use_auth_token)
+            print(self.config.download_mode)
+            print(self.config.cache_dir)
             setattr(
                 self,
                 name_dataset,
